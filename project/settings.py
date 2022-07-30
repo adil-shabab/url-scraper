8d23e057937c2666bcd18079c2f0d1738619b789
Adil Shabab <adilshaba@gmail.com>
2022-07-30 11:19:50 -0700

    media link created

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
