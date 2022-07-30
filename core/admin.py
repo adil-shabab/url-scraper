ac831be61e5c8abdaa75288edd704a61c7a171e0
Adil Shabab <adilshaba@gmail.com>
2022-07-30 11:24:10 -0700

    registered in admin (models)

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Website, Images, Url
 # Register your models here.
+
+
+admin.site.register(Website)
+admin.site.register(Url)
+admin.site.register(Images)
