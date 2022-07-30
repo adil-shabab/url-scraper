a82b455edba079d7ad9202e32ffba9ed5eaa0df3
Adil Shabab <adilshaba@gmail.com>
2022-07-30 07:14:27 -0700

    started word count logic

--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
+from bs4 import BeautifulSoup
 from django.shortcuts import redirect, render
+import requests
 
 # Create your views here.
 def home(request):
-    if request.method == "GET":
-        input = request.GET.get('input')
-        print(input)
+    if request.method == "POST":
+        link = request.POST.get('input')
+        r = requests.get(link)
+        print(r)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        print(soup)
+
+
+        # ********************* word count *******************
+        word_count = 0
+        source_code = requests.get(link).text
+        print(source_code)
     return render(request, 'index.html')
