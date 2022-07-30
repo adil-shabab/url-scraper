0b0429597b922eeadb8d2273578e3a52521eb03a
Adil Shabab <adilshaba@gmail.com>
2022-07-30 06:33:35 -0700

    input value got

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def home(request):
+    if request.method == "GET":
+        input = request.GET.get('input')
+        print(input)
     return render(request, 'index.html')
