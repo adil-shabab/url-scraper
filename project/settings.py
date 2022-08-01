edb6b8461665f28703c6f8d5802016d5ffc020f4
Adil Shabab <adilshaba@gmail.com>
2022-08-01 00:21:41 -0700

    allowed host changed

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ths&(pwh+p#1^y3p=jfho+b(7$fdwvh49#*d3b73$xgj#uxb#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['url--scraper.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
